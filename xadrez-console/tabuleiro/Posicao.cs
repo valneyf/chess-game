8685cc67e26d43f04190f84a6c478b4fe871f966
Valney Filho <fco.valney@gmail.com>
2020-12-29 20:01:54 -0300

    Created Posicao class

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+﻿namespace tabuleiro
+{
+    class Posicao
+    {
+        public int row { get; set; }
+        public int column { get; set; }
+
+        public Posicao(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return row
+                + ", "
+                + column;
+        }
+    }
+}
